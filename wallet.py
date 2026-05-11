48106653a06d9ba90c85aaeb3ca78467e409e7f4
andreas17-alt <andreas.randriamorasata@student.passerellesnumeriques.org>
2026-05-11 15:42:12 +0100

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,6 +1,6 @@
 class Wallet:
     def __init__(self, balance):
-        self.balance = balancer
+        self.balance = balancerr
 
     def set_balance(self, val):
         self.balance = self.balance + val
